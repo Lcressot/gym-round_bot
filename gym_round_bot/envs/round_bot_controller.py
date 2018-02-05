ad2aa4081a4068f7688c3db88ce582911b5c704e
Loic <lcressot@gmail.com>
2018-02-05 11:41:34 +0100

    new XZ controller

--- gym_round_bot/envs/round_bot_controller.py
+++ gym_round_bot/envs/round_bot_controller.py
@@ -26,7 +26,7 @@ class Simple_TetaSpeed_Controller(Controller):
 	This class controls the robot with fixed dteta rotations and fixed speed forward move
 	"""
 	def __init__(self, model, dteta, speed):
-		super(Simple_TetaSpeed_Controller,self).__init__(model,"TetaSpeed")
+		super(Simple_TetaSpeed_Controller,self).__init__(model,"Simple_TetaSpeed")
 		self.dteta = dteta
 		self.model.walking_speed = speed
 		self.action_meaning = {
@@ -42,7 +42,37 @@ class Simple_TetaSpeed_Controller(Controller):
         3 : "self.model.change_robot_rotation(-self.dteta,0)",
 		}
 
+	@speed.setter
+	def speed(self, s):
+		self.model.walking_speed = s
+
 	@property
+	def speed(self):
+		return self.model.walking_speed
+
+
+
+class Simple_XZ_Controller(Controller):
+	"""
+	This class controls the robot to move on (oXZ) plan, always looking in the same direction
+	"""
+	def __init__(self, model, speed):
+		super(Simple_XZ_Controller,self).__init__(model,"Simple_XZ")
+		self.model.walking_speed = speed
+		self.action_meaning = {
+		0 : "MOVE FOWARD",
+        1 : "MOVE BACKWARD",
+        2 : "MOVE RIGHT",
+        3 : "MOVE LEFT", 
+		}
+		self.actions = {
+		0 : "self.model.strafe[0]=-1", #-1 is forward
+        1 : "self.model.strafe[0]=1", # move backward
+        2 : "self.model.strafe[1]=1",
+        3 : "self.model.strafe[0]=-1",
+		}
+
+	@speed.setter
 	def speed(self, s):
 		self.model.walking_speed = s
 
