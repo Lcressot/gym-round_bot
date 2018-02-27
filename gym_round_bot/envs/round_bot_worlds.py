40726588e939838090812d2a9faea84861ce8ea8
Loic <lcressot@gmail.com>
2018-02-27 13:14:12 +0100

    small update to parametrize reward values in worlds

--- gym_round_bot/envs/round_bot_worlds.py
+++ gym_round_bot/envs/round_bot_worlds.py
@@ -18,6 +18,7 @@ def _build_rb1_default_world(model, width=20, hwalls=5, dwalls=1,
                     texture_bricks='/textures/texture_test.png',
                     texture_robot='/textures/robot.png',
                     texture_visualisation='/textures/visualisation.png',
+                    wall_reward=-1,
                     ):
     """
     Builds a simple rectangle planar world with walls around
@@ -28,7 +29,8 @@ def _build_rb1_default_world(model, width=20, hwalls=5, dwalls=1,
     # dwalls is depth of walls
 
     """
-    from gym_round_bot.envs import round_bot_model
+    # TODO : better import would be global and without "from" but doesn't work for the moment
+    from gym_round_bot.envs import round_bot_model 
     # create textures coordinates
     GRASS = round_bot_model.Block.tex_coords((1, 0), (0, 1), (0, 0))
     MUD = round_bot_model.Block.tex_coords((0, 1), (0, 1), (0, 1))
@@ -54,13 +56,13 @@ def _build_rb1_default_world(model, width=20, hwalls=5, dwalls=1,
 
     # Build wall blocks with negative reward on collision
     #back wall
-    model.add_block( (0, hwalls/2, -n, wwalls, hwalls, dwalls, 0.0, 0.0, 0.0), STONE, collision_reward = -1)
+    model.add_block( (0, hwalls/2, -n, wwalls, hwalls, dwalls, 0.0, 0.0, 0.0), STONE, collision_reward = wall_reward)
     #front wall
-    model.add_block( (0, hwalls/2, n, wwalls, hwalls, dwalls, 0.0, 0.0, 0.0), BRICK, collision_reward = -1)
+    model.add_block( (0, hwalls/2, n, wwalls, hwalls, dwalls, 0.0, 0.0, 0.0), BRICK, collision_reward = wall_reward)
     #left wall
-    model.add_block( (-n, hwalls/2, 0, dwalls, hwalls, wwalls, 0.0, 0.0, 0.0), SAND, collision_reward = -1)
+    model.add_block( (-n, hwalls/2, 0, dwalls, hwalls, wwalls, 0.0, 0.0, 0.0), SAND, collision_reward = wall_reward)
     #right wall
-    model.add_block( (n, hwalls/2, 0, dwalls, hwalls, wwalls, 0.0, 0.0, 0.0), MUD, collision_reward = -1)    
+    model.add_block( (n, hwalls/2, 0, dwalls, hwalls, wwalls, 0.0, 0.0, 0.0), MUD, collision_reward = wall_reward)    
 
     world_info = {  'width' : 2*n,
                     'depth' : 2*n,
@@ -68,12 +70,14 @@ def _build_rb1_default_world(model, width=20, hwalls=5, dwalls=1,
 
     return texture_paths, world_info
 
-def build_rb1_world(model, width=20, hwalls=5, dwalls=1):
+
+
+def build_rb1_world(model, width=20, hwalls=5, dwalls=1, wall_reward=-1, goal_reward=1):
     """
     Builds the rb1 world
     """    
     ## first build default world
-    texture_paths, world_info = _build_rb1_default_world(model,width=width, hwalls=hwalls, dwalls=dwalls)
+    texture_paths, world_info = _build_rb1_default_world(model,width=width, hwalls=hwalls, dwalls=dwalls, wall_reward=wall_reward)
 
     ## then add specs
     from gym_round_bot.envs import round_bot_model
@@ -90,21 +94,23 @@ def build_rb1_world(model, width=20, hwalls=5, dwalls=1):
     bot_height = 1
 
     # Build reward block in the corner
-    model.add_block( (n-(wr/2+dwalls/2), bot_height/2.0, -n+(wr/2+dwalls/2), wr, bot_height/3.0, wr, 0.0, 0.0, 0.0), REWARD, block_type='reward', collision_reward = 1)
+    model.add_block( (n-(wr/2+dwalls/2), bot_height/2.0, -n+(wr/2+dwalls/2), wr, bot_height/3.0, wr, 0.0, 0.0, 0.0), REWARD, block_type='reward', collision_reward = goal_reward)
     # Build robot block, set initial height to bot_heigh/2 + small offset to avoid ground collision
     model.add_block( (0, bot_height/2.0+0.1, 0, 2*bot_diameter, bot_height, 2*bot_diameter, 0.0, 0.0, 0.0), BOT, block_type='robot')
     # add starting areas (the height=0 of block does not matter here, only area of (hwalls-2*dwalls)^2)
-    model.add_block( (0, bot_height/2.0+0.1, 0, wwalls-2*dwalls, 1, hwalls-2*dwalls, 0.0, 0.0, 0.0), START, block_type='start')
+    model.add_block( (0, bot_height/2.0+0.1, 0, wwalls-2*dwalls, 0.1, wwalls-2*dwalls, 0.0, 0.0, 0.0), START, block_type='start')
 
     return texture_paths, world_info
 
-def build_rb1_1wall_world(model, width=20, hwalls=2, dwalls=2):
+
+
+def build_rb1_1wall_world(model, width=20, hwalls=2, dwalls=2, wall_reward=-1, goal_reward=1):
     """
     Builds a simple rectangle planar world with walls around, and 1 wall in the middle
     Return : world information
     """
     ## first build default world
-    texture_paths, world_info = _build_rb1_default_world(model,width=width, hwalls=hwalls, dwalls=dwalls)
+    texture_paths, world_info = _build_rb1_default_world(model,width=width, hwalls=hwalls, dwalls=dwalls, rew_walls=rew_walls, wall_reward=wall_reward)
 
     ## then add specs
     from gym_round_bot.envs import round_bot_model
