47b97e2a81e84218989a7a6c55d6ee3b0f4373a1
Loic <lcressot@gmail.com>
2018-12-12 17:48:18 +0100

    added library pillow to required libraries, usefull on linux systems

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from setuptools import setup
 setup(
 	name='gym_round_bot',
 	version='0.0.1',    
-	install_requires=['numpy>=1.10.4','pyglet>=1.2.0','scipy', ],
+	install_requires=['numpy>=1.10.4','pyglet>=1.2.0','scipy', 'pillow' ],
 	entry_point='gym_round_bot.envs',
 	author='Loic Cressot',
+    author_email="Lcressot@gmail.com",
+    description="OpenAI gym environment for robotic simulation. Simple round bot driving in a 3D maze-type world with walls. Compatible with both Python 2 and 3.",
+    url="https://github.com/Lcressot/gym-round_bot",
+    license='MIT',
 	classifiers=[
     # How mature is this project? Common values are
     #   3 - Alpha
